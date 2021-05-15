bf4248353e9cd20b9ff41715e2bec0752753a254
Aryan Karan <aryankaran28022004@gmail.com>
2021-05-15 17:55:04 +0530

    Removed Old Vendor Blobs

--- Cloud_Q11-vendor.mk
+++
@@ -1,9 +0,0 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/intex/Cloud_Q11/bin,system/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/intex/Cloud_Q11/lib,system/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/intex/Cloud_Q11/etc,system/etc)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/intex/Cloud_Q11/vendor,system/vendor)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/intex/Cloud_Q11/xbin,system/xbin)
