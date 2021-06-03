5990ad854409f04669989c0bb7fd4017fb52fa36
Aryan Karan <76065891+aryankaran@users.noreply.github.com>
2021-06-04 00:57:11 +0530

    Update cm_Cloud_Q11.mk

--- cm_Cloud_Q11.mk
+++ cm_Cloud_Q11.mk
@@ -12,8 +12,8 @@ TARGET_SCREEN_WIDTH := 720
 
 # Device identifier. This must come after all inclusions
 
-PRODUCT_DEVICE := Cloud_Q11
 PRODUCT_NAME := cm_Cloud_Q11
+PRODUCT_DEVICE := Cloud_Q11
 PRODUCT_BRAND := Intex
-PRODUCT_MODEL := Cloud_Q11
-PRODUCT_MANUFACTURER := intex
+PRODUCT_MODEL := Cloud Q11
+PRODUCT_MANUFACTURER := Intex
